0b1dcc1492b078670a1f6cba02108f2cf9068501
Gabor Somogyi <gabor.somogyi@atolldev.hu>
2022-02-07 15:39:17 +0100

    Init

---
+++ fizz-buzz-web/src/main/java/hu/sprintform/gsomogyi/fizz/buzz/FizzBuzzApplication.java
@@ -0,0 +1,12 @@
+package hu.sprintform.gsomogyi.fizz.buzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FizzBuzzApplication.class, args);
+    }
+}
